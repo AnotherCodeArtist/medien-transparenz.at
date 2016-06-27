455002c3c60cd3f512d6c41cc8f873dfbcf6a3d6
Peter Salhofer <psalhofer@gmail.com>
2016-06-27 16:18:58 +0200

    initial attempt to integrate threeDcharts

--- packages/custom/transparency/app.js
+++ packages/custom/transparency/app.js
@@ -103,6 +103,12 @@ Transparency.register(function(app, auth, database) {
   Transparency.aggregateAsset('js', '../lib/ng-file-upload/ng-file-upload.js',{weight:-5});
   Transparency.aggregateAsset('js', '../lib/d3/d3.js',{weight:-4});
   Transparency.aggregateAsset('js', '../lib/nvd3/build/nv.d3.js',{weight:-3});
+  Transparency.aggregateAsset('js', '../lib/three.js/three.js',{weight:-4});
+  Transparency.aggregateAsset('js', '../lib/topojson/topojson.js',{weight:-4});
+  Transparency.aggregateAsset('js', '../lib/turfjs/turf.min.js',{weight:-1});
+  Transparency.aggregateAsset('js', '../js/TrackballControls.js',{weight:-3});
+  Transparency.aggregateAsset('js', '../js/Detector.js',{weight:-3});
+  Transparency.aggregateAsset('js', '../js/geo.js',{weight:-2});
   Transparency.aggregateAsset('js', '../lib/datatables/media/js/jquery.dataTables.js',{weight:-3});
   Transparency.aggregateAsset('js', '../lib/d3-plugins-sankey/sankey.js',{weight:-2});
   Transparency.aggregateAsset('js', '../lib/angular-datatables/dist/angular-datatables.js',{weight:-2});
